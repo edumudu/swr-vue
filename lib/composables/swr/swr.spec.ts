d294519543bf6aa30cb4b9d5e2fdf7ab694562a9
Eduardo Wesley <eduardomudutiu@gmail.com>
2022-07-09 13:35:07 -0300

    feat: serialize keys and not fetch is key is falsy (#5)
    * feat: create function to hash any js primitive value
    
    * feat: create check types utils
    
    * chore: add vitest globals types and change prettir printWidth to 100 characters
    
    * feat: create serialize key function
    
    * feat: return empty string as key when falsy value or throw
    
    * feat: use serialize key in useSWR

--- lib/composables/swr/swr.spec.ts
+++ lib/composables/swr/swr.spec.ts
@@ -35,49 +35,36 @@ describe('useSWR', () => {
     [() => 1],
     [() => ({ id: 2 })],
     [async () => Promise.resolve('returnedData')],
-  ])(
-    'should set data returned from fetcher to data variable',
-    async (fetcher) => {
-      const [result] = withSetup(() =>
-        useSWR<any>(defaultKey, fetcher, defaultOptions),
-      );
-      const { data, isValidating } = result;
+  ])('should set data returned from fetcher to data variable', async (fetcher) => {
+    const [result] = withSetup(() => useSWR<any>(defaultKey, fetcher, defaultOptions));
+    const { data, isValidating } = result;
 
-      expect(data.value).toEqual(undefined);
+    expect(data.value).toEqual(undefined);
 
-      await flushPromises();
-      expect(data.value).toEqual(await fetcher());
-      expect(isValidating.value).toBeFalsy();
-    },
-  );
+    await flushPromises();
+    expect(data.value).toEqual(await fetcher());
+    expect(isValidating.value).toBeFalsy();
+  });
 
   it.each([
     // eslint-disable-next-line prefer-promise-reject-errors
     [() => Promise.reject('Error in fetcher'), 'Error in fetcher'],
-    [
-      () => Promise.reject(new Error('Error in fetcher')),
-      new Error('Error in fetcher'),
-    ],
+    [() => Promise.reject(new Error('Error in fetcher')), new Error('Error in fetcher')],
     [
       () => {
         throw new Error('Error in fetcher');
       },
       new Error('Error in fetcher'),
     ],
-  ])(
-    'should set error when throw error in fetcher',
-    async (fetcher, expectedError) => {
-      const [result] = withSetup(() =>
-        useSWR(defaultKey, fetcher, defaultOptions),
-      );
-      const { data, isValidating, error } = result;
+  ])('should set error when throw error in fetcher', async (fetcher, expectedError) => {
+    const [result] = withSetup(() => useSWR(defaultKey, fetcher, defaultOptions));
+    const { data, isValidating, error } = result;
 
-      await flushPromises();
-      expect(data.value).toEqual(undefined);
-      expect(error.value).toEqual(expectedError);
-      expect(isValidating.value).toBeFalsy();
-    },
-  );
+    await flushPromises();
+    expect(data.value).toEqual(undefined);
+    expect(error.value).toEqual(expectedError);
+    expect(isValidating.value).toBeFalsy();
+  });
 
   it('should return cached value first then revalidate', async () => {
     vi.useFakeTimers();
@@ -88,9 +75,7 @@ describe('useSWR', () => {
         setTimeout(() => resolve('FetcherResult'), 1000);
       });
 
-    const [result] = withSetup(() =>
-      useSWR(defaultKey, fetcher, defaultOptions),
-    );
+    const [result] = withSetup(() => useSWR(defaultKey, fetcher, defaultOptions));
     const { data } = result;
 
     expect(data.value).toBe('cachedData');
@@ -106,9 +91,7 @@ describe('useSWR', () => {
     cacheProvider.set(defaultKey, 'cachedData');
 
     const fetcher = vi.fn().mockResolvedValue('FetcherResult');
-    const [result] = withSetup(() =>
-      useSWR(defaultKey, fetcher, defaultOptions),
-    );
+    const [result] = withSetup(() => useSWR(defaultKey, fetcher, defaultOptions));
     const { data } = result;
 
     const blurEvent = new Event('blur', { bubbles: true });
@@ -126,9 +109,7 @@ describe('useSWR', () => {
     const key = ref('initialKey');
     const fetcher = vi.fn();
 
-    withSetup(() =>
-      useSWR(() => `testing-key/${key.value}`, fetcher, defaultOptions),
-    );
+    withSetup(() => useSWR(() => `testing-key/${key.value}`, fetcher, defaultOptions));
 
     expect(fetcher).toBeCalledTimes(1);
 
@@ -136,5 +117,58 @@ describe('useSWR', () => {
 
     await flushPromises();
     expect(fetcher).toBeCalledTimes(2);
+  });
+
+  it('should recall fetcher if key threw and change to valid value', async () => {
+    const key = ref();
+    const fetcher = vi.fn();
+
+    withSetup(() => useSWR(() => `testing-key/${key.value.id}`, fetcher, defaultOptions));
+
+    expect(fetcher).toBeCalledTimes(0);
+
+    key.value = { id: 1 };
+
+    await flushPromises();
+    expect(fetcher).toBeCalledTimes(1);
+  });
+
+  it.each([
+    '/user/me',
+    'https://google.com',
+    ['/api/user', 4, 'authKey'],
+    () => '/user/me',
+    () => 'https://google.com',
+    () => ['/api/user', 4, 'authKey'],
+  ])('should call fetcher function passing keys as arguments: %#', async (key) => {
+    const fetcher = vi.fn();
+    let expectedArgs = typeof key === 'function' ? key() : key;
+
+    expectedArgs = Array.isArray(expectedArgs) ? expectedArgs : [expectedArgs];
+
+    withSetup(() => useSWR(key, fetcher, defaultOptions));
+
+    expect(fetcher).toHaveBeenCalled();
+    expect(fetcher).toHaveBeenCalledWith(...expectedArgs);
+  });
+
+  it.each([
+    undefined,
+    false as const,
+    null,
+    '',
+    () => undefined,
+    () => false as const,
+    () => null,
+    () => '',
+    () => {
+      throw new Error('Mock Error');
+    },
+  ])('should not call fetcher if key is/return falsy value or throw: %#', async (key) => {
+    const fetcher = vi.fn();
+
+    withSetup(() => useSWR(key, fetcher, defaultOptions));
+
+    expect(fetcher).not.toHaveBeenCalled();
   });
 });
