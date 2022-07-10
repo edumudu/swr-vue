68b2d41fd1e5f30f527d30555e8235ddf855b3f0
Eduardo Wesley <eduardomudutiu@gmail.com>
2022-07-09 22:34:06 -0300

    refactor: split types to better reutilization

--- lib/types/lib.ts
+++ lib/types/lib.ts
@@ -7,3 +7,18 @@ export type KeyArguments =
   | false;
 
 export type Key = KeyArguments | (() => KeyArguments);
+
+export interface CacheProvider<Data = any> {
+  keys(): IterableIterator<string>;
+  has(key: Key): boolean;
+  get(key: Key): Data | undefined;
+  set(key: Key, value: Data): void;
+  delete(key: Key): void;
+}
+
+export type SWRConfig = {
+  cacheProvider?: CacheProvider;
+  revalidateOnFocus?: boolean;
+  revalidateOnReconnect?: boolean;
+  revalidateIfStale?: boolean;
+};
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './check-types';
 export * from './hash';
 export * from './serialize-key';
+export * from './merge-config';
