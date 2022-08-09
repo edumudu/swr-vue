99e49dc3d171ddb1a14df0b77c23d4bba0b8d7b4
edumudu <eduardomudutiu@gmail.com>
2022-08-08 21:09:38 -0300

    feat: implement logic to merge callbacks in config

--- lib/utils/merge-config/merge-config.spec.ts
+++ lib/utils/merge-config/merge-config.spec.ts
@@ -20,4 +20,46 @@ describe('mergeConfig', () => {
   ])('should merge two configs: %s', (obj1, obj2, expectedResult) => {
     expect(mergeConfig(obj1, obj2)).toEqual(expectedResult);
   });
+
+  it('should also merge callbacks', () => {
+    const globalConfig = Object.freeze({
+      onError: vi.fn(),
+      onSuccess: vi.fn(),
+    });
+
+    const localConfig = Object.freeze({
+      onError: vi.fn(),
+      onSuccess: vi.fn(),
+    });
+
+    const { onError, onSuccess } = mergeConfig(globalConfig, localConfig);
+
+    onError();
+    onSuccess();
+
+    expect(globalConfig.onSuccess).toHaveBeenCalledOnce();
+    expect(globalConfig.onError).toHaveBeenCalledOnce();
+    expect(localConfig.onSuccess).toHaveBeenCalledOnce();
+    expect(localConfig.onError).toHaveBeenCalledOnce();
+  });
+
+  it('should not throw when one of the callbacks is missing', () => {
+    const globalConfig = Object.freeze({ onSuccess: vi.fn() });
+    const localConfig = Object.freeze({ onError: vi.fn() });
+
+    const { onError, onSuccess } = mergeConfig(globalConfig, localConfig);
+
+    onError();
+    onSuccess();
+
+    expect(globalConfig.onSuccess).toHaveBeenCalledOnce();
+    expect(localConfig.onError).toHaveBeenCalledOnce();
+  });
+
+  it('should return undefined when callback is missing in either configs', () => {
+    const { onError, onSuccess } = mergeConfig({}, {});
+
+    expect(onError).toBeUndefined();
+    expect(onSuccess).toBeUndefined();
+  });
 });
